0f6b8dde5f0959d3ec88a2f4e887cd540e952a26
Kur-Achan <achananyiethkur@gmail.com>
2024-02-08 22:32:07 +0300

    add a new file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,12 @@
 fun main(){
-var name :string = "Kur Achan"
-    var age : Byte 22
-    var = phonenumber : " +254742681452"
-    var  weight : float = 72.6
-    var citizen : Boolean = true
+var name  = "Kur Achan"
+    var age = 22
+    var phonenumber= 254742681452
+    var  weight = 72.6F
+    var citizen = true
+
+    println(name)
+    println(age)
+    println(phonenumber)
+    println(weight)
+    println(citizen)
