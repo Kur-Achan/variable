98e27e0d755cdbf5f1a9be4c349830e8085c9a6d
Kur-Achan <achananyiethkur@gmail.com>
2024-02-09 07:05:41 +0300

    add a variable

--- src/main.kt
+++ src/main.kt
@@ -1,12 +1,12 @@
 fun main(){
-var name  = "Kur Achan"
+    var name  = "Kur Achan"
     var age = 22
-    var phonenumber= 254742681452
+    var phoneNumber = 254742681452
     var  weight = 72.6F
     var citizen = true
 
     println(name)
     println(age)
-    println(phonenumber)
+    println(phoneNumber)
     println(weight)
     println(citizen)
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,12 @@
+fun main(){
+    var name  = "Kur Achan"
+    var age = 22
+    var phoneNumber = 254742681452
+    var  weight = 72.6F
+    var citizen = true
+
+    println(name)
+    println(age)
+    println(phoneNumber)
+    println(weight)
+    println(citizen)
